3ab663f3013e3856ab5a907700591cdbf83f7457
Michael <michaeldrpabaya@gmail.com>
2022-11-24 00:19:37 +0800

    Remove client button and redirect client on login form

--- WWWPOS/Form1.Designer.cs
+++ WWWPOS/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.btn_SignUp = new System.Windows.Forms.Button();
-            this.btn_Client = new System.Windows.Forms.Button();
             this.btn_Login = new System.Windows.Forms.Button();
             this.txtBox_Password = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -49,7 +48,6 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(53)))), ((int)(((byte)(165)))));
             this.panel1.Controls.Add(this.btn_SignUp);
-            this.panel1.Controls.Add(this.btn_Client);
             this.panel1.Controls.Add(this.btn_Login);
             this.panel1.Controls.Add(this.txtBox_Password);
             this.panel1.Controls.Add(this.label2);
@@ -58,9 +56,10 @@
             this.panel1.Controls.Add(this.Tagline);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.panel1.Location = new System.Drawing.Point(619, 59);
+            this.panel1.Location = new System.Drawing.Point(825, 73);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(406, 600);
+            this.panel1.Size = new System.Drawing.Size(541, 738);
             this.panel1.TabIndex = 4;
             // 
             // btn_SignUp
@@ -70,28 +69,14 @@
             this.btn_SignUp.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_SignUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_SignUp.ForeColor = System.Drawing.Color.White;
-            this.btn_SignUp.Location = new System.Drawing.Point(236, 422);
+            this.btn_SignUp.Location = new System.Drawing.Point(295, 519);
+            this.btn_SignUp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_SignUp.Name = "btn_SignUp";
-            this.btn_SignUp.Size = new System.Drawing.Size(106, 31);
+            this.btn_SignUp.Size = new System.Drawing.Size(161, 46);
             this.btn_SignUp.TabIndex = 8;
             this.btn_SignUp.Text = "Sign up";
             this.btn_SignUp.UseVisualStyleBackColor = false;
             this.btn_SignUp.Click += new System.EventHandler(this.btn_SignUp_Click);
-            // 
-            // btn_Client
-            // 
-            this.btn_Client.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(19)))), ((int)(((byte)(30)))));
-            this.btn_Client.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btn_Client.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_Client.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_Client.ForeColor = System.Drawing.Color.White;
-            this.btn_Client.Location = new System.Drawing.Point(120, 501);
-            this.btn_Client.Name = "btn_Client";
-            this.btn_Client.Size = new System.Drawing.Size(197, 42);
-            this.btn_Client.TabIndex = 7;
-            this.btn_Client.Text = "Client";
-            this.btn_Client.UseVisualStyleBackColor = false;
-            this.btn_Client.Click += new System.EventHandler(this.btn_Client_Click);
             // 
             // btn_Login
             // 
@@ -100,9 +85,10 @@
             this.btn_Login.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btn_Login.ForeColor = System.Drawing.Color.White;
-            this.btn_Login.Location = new System.Drawing.Point(89, 422);
+            this.btn_Login.Location = new System.Drawing.Point(119, 519);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_Login.Name = "btn_Login";
-            this.btn_Login.Size = new System.Drawing.Size(106, 31);
+            this.btn_Login.Size = new System.Drawing.Size(161, 46);
             this.btn_Login.TabIndex = 6;
             this.btn_Login.Text = "Log in";
             this.btn_Login.UseVisualStyleBackColor = false;
@@ -114,20 +100,21 @@
             this.txtBox_Password.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBox_Password.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtBox_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBox_Password.Location = new System.Drawing.Point(89, 376);
+            this.txtBox_Password.Location = new System.Drawing.Point(119, 463);
+            this.txtBox_Password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBox_Password.Multiline = true;
             this.txtBox_Password.Name = "txtBox_Password";
-            this.txtBox_Password.Size = new System.Drawing.Size(253, 25);
+            this.txtBox_Password.Size = new System.Drawing.Size(337, 31);
             this.txtBox_Password.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(126, 346);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(168, 426);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(181, 27);
+            this.label2.Size = new System.Drawing.Size(241, 33);
             this.label2.TabIndex = 4;
             this.label2.Text = "Password";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -138,20 +125,21 @@
             this.txtBox_Email.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBox_Email.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtBox_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBox_Email.Location = new System.Drawing.Point(89, 313);
+            this.txtBox_Email.Location = new System.Drawing.Point(119, 385);
+            this.txtBox_Email.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBox_Email.Multiline = true;
             this.txtBox_Email.Name = "txtBox_Email";
-            this.txtBox_Email.Size = new System.Drawing.Size(253, 25);
+            this.txtBox_Email.Size = new System.Drawing.Size(337, 31);
             this.txtBox_Email.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(126, 283);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(168, 348);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 27);
+            this.label1.Size = new System.Drawing.Size(241, 33);
             this.label1.TabIndex = 2;
             this.label1.Text = "Email";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -160,10 +148,10 @@
             // 
             this.Tagline.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Tagline.ForeColor = System.Drawing.Color.White;
-            this.Tagline.Location = new System.Drawing.Point(37, 193);
-            this.Tagline.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Tagline.Location = new System.Drawing.Point(49, 238);
+            this.Tagline.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.Tagline.Name = "Tagline";
-            this.Tagline.Size = new System.Drawing.Size(351, 73);
+            this.Tagline.Size = new System.Drawing.Size(468, 90);
             this.Tagline.TabIndex = 1;
             this.Tagline.Text = "Tagline Lorem Ipsum dolor Sit amet Contol sit lea adaodjdjd Blahbahblah Ndwbofosi" +
     "do fso dcosldvicab";
@@ -173,10 +161,10 @@
             // 
             this.panel2.BackgroundImage = global::WWWPOS.Properties.Resources.Logo;
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.Location = new System.Drawing.Point(143, 59);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Location = new System.Drawing.Point(191, 73);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(145, 129);
+            this.panel2.Size = new System.Drawing.Size(193, 159);
             this.panel2.TabIndex = 0;
             // 
             // panel3
@@ -184,21 +172,22 @@
             this.panel3.BackgroundImage = global::WWWPOS.Properties.Resources.header;
             this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel3.Location = new System.Drawing.Point(42, 293);
+            this.panel3.Location = new System.Drawing.Point(56, 361);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(531, 205);
+            this.panel3.Size = new System.Drawing.Size(707, 251);
             this.panel3.TabIndex = 5;
             // 
             // LoginPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(19)))), ((int)(((byte)(30)))));
-            this.ClientSize = new System.Drawing.Size(1040, 640);
+            this.ClientSize = new System.Drawing.Size(1387, 788);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "LoginPage";
             this.Padding = new System.Windows.Forms.Padding(0, 1, 0, 0);
             this.ShowIcon = false;
@@ -221,7 +210,6 @@
         private System.Windows.Forms.Label Tagline;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button btn_Client;
         private System.Windows.Forms.Button btn_SignUp;
     }
 }
