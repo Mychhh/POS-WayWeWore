517121e3630438a52beec1f526bb9833027d9cb1
Michael <michaeldrpabaya@gmail.com>
2022-12-03 20:32:37 +0800

    rebase fixed conflict

--- WWWPOS/Program.cs
+++ WWWPOS/Program.cs
@@ -16,7 +16,7 @@ namespace WWWPOS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginPage());
+            Application.Run(new Form2());
         }
     }
 }
