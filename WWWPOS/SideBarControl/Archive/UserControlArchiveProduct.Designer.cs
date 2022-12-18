a0614dc60e9a03d2782cdba5eadb49e2b5049441
Michael <michaeldrpabaya@gmail.com>
2022-12-18 14:59:26 +0800

    Archive Product User Control Refresh After Restore

--- WWWPOS/SideBarControl/Archive/UserControlArchiveProduct.Designer.cs
+++ WWWPOS/SideBarControl/Archive/UserControlArchiveProduct.Designer.cs
@@ -43,7 +43,7 @@
             this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.restore = new System.Windows.Forms.DataGridViewImageColumn();
+            this.restoreProduct = new System.Windows.Forms.DataGridViewImageColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid_ArchiveProduct)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,10 +51,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 4);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(19, 5);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 24);
+            this.label2.Size = new System.Drawing.Size(96, 29);
             this.label2.TabIndex = 35;
             this.label2.Text = "Product";
             // 
@@ -82,7 +81,7 @@
             this.dataGridViewTextBoxColumn8,
             this.dataGridViewTextBoxColumn9,
             this.Column1,
-            this.restore});
+            this.restoreProduct});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -92,7 +91,8 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGrid_ArchiveProduct.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGrid_ArchiveProduct.EnableHeadersVisualStyles = false;
-            this.dataGrid_ArchiveProduct.Location = new System.Drawing.Point(18, 31);
+            this.dataGrid_ArchiveProduct.Location = new System.Drawing.Point(24, 38);
+            this.dataGrid_ArchiveProduct.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGrid_ArchiveProduct.Name = "dataGrid_ArchiveProduct";
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
@@ -103,7 +103,8 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGrid_ArchiveProduct.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGrid_ArchiveProduct.RowHeadersVisible = false;
-            this.dataGrid_ArchiveProduct.Size = new System.Drawing.Size(656, 339);
+            this.dataGrid_ArchiveProduct.RowHeadersWidth = 51;
+            this.dataGrid_ArchiveProduct.Size = new System.Drawing.Size(875, 417);
             this.dataGrid_ArchiveProduct.TabIndex = 36;
             this.dataGrid_ArchiveProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_ArchiveProduct_CellContentClick);
             this.dataGrid_ArchiveProduct.SelectionChanged += new System.EventHandler(this.dataGrid_ArchiveProduct_SelectionChanged);
@@ -111,77 +112,91 @@
             // dataGridViewTextBoxColumn1
             // 
             this.dataGridViewTextBoxColumn1.HeaderText = "Product ID";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
             // 
             // dataGridViewTextBoxColumn2
             // 
             this.dataGridViewTextBoxColumn2.HeaderText = "Category";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
             // 
             // dataGridViewTextBoxColumn3
             // 
             this.dataGridViewTextBoxColumn3.HeaderText = "Product Name";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
             this.dataGridViewTextBoxColumn3.Width = 115;
             // 
             // dataGridViewTextBoxColumn4
             // 
             this.dataGridViewTextBoxColumn4.HeaderText = "Product Color";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
             this.dataGridViewTextBoxColumn4.Width = 115;
             // 
             // dataGridViewTextBoxColumn5
             // 
             this.dataGridViewTextBoxColumn5.HeaderText = "Product Price";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
             this.dataGridViewTextBoxColumn5.Width = 115;
             // 
             // dataGridViewTextBoxColumn6
             // 
             this.dataGridViewTextBoxColumn6.HeaderText = "Product Stock";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
             this.dataGridViewTextBoxColumn6.Width = 115;
             // 
             // dataGridViewTextBoxColumn7
             // 
             this.dataGridViewTextBoxColumn7.HeaderText = "Product Size";
+            this.dataGridViewTextBoxColumn7.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
             this.dataGridViewTextBoxColumn7.Width = 115;
             // 
             // dataGridViewTextBoxColumn8
             // 
             this.dataGridViewTextBoxColumn8.HeaderText = "Product Description";
+            this.dataGridViewTextBoxColumn8.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
             this.dataGridViewTextBoxColumn8.Width = 130;
             // 
             // dataGridViewTextBoxColumn9
             // 
             this.dataGridViewTextBoxColumn9.HeaderText = "Product Status";
+            this.dataGridViewTextBoxColumn9.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
             this.dataGridViewTextBoxColumn9.Width = 115;
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Upload_at";
+            this.Column1.MinimumWidth = 6;
             this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
             // 
-            // restore
+            // restoreProduct
             // 
-            this.restore.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
-            this.restore.HeaderText = "";
-            this.restore.Image = global::WWWPOS.Properties.Resources.restore;
-            this.restore.Name = "restore";
-            this.restore.Width = 5;
+            this.restoreProduct.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.restoreProduct.HeaderText = "Restore";
+            this.restoreProduct.Image = global::WWWPOS.Properties.Resources.restore;
+            this.restoreProduct.MinimumWidth = 6;
+            this.restoreProduct.Name = "restoreProduct";
+            this.restoreProduct.Width = 71;
             // 
             // UserControlArchiveProduct
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dataGrid_ArchiveProduct);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "UserControlArchiveProduct";
-            this.Size = new System.Drawing.Size(699, 420);
+            this.Size = new System.Drawing.Size(932, 517);
             this.Load += new System.EventHandler(this.UserControlArchiveProduct_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid_ArchiveProduct)).EndInit();
             this.ResumeLayout(false);
@@ -203,6 +218,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewImageColumn restore;
+        private System.Windows.Forms.DataGridViewImageColumn restoreProduct;
     }
 }
