7d51d4cdd46a57550a82fbd5654be6ad346627a4
Michael <michaeldrpabaya@gmail.com>
2022-12-26 17:36:08 +0800

    create another file for loadData

--- WWWPOS/Class_Products.cs
+++ WWWPOS/Class_Products.cs
@@ -14,24 +14,19 @@ namespace WWWPOS
         public int Account_ID { get; set; }
         public string Product_Category { get; set; }
         public string Product_Name { get; set; }
-        public ArrayList Product_Color = new ArrayList();
+        public string Product_Color { get; set; }
         public double Product_Price { get; set; }
         public int Product_Stock { get; set; }
         public string Product_Images { get; set; }
-        public ArrayList Product_Size = new ArrayList();
+        public string Product_Size { get; set; }
         public string Product_Descripiton { get; set; }
         public string Product_Status { get; set; }
         public string Product_Added_At { get; set; }
 
-        public Class_Products()
-        {
-
-        }
-
         //Contructors
-        public Class_Products(int productID, int accountID, string productCategory, string productName, 
-                       ArrayList productColor, double productPrice, int productStock, 
-                       string productImages, ArrayList productSize, string productDescription,
+        public Class_Products(int productID, int accountID, string productCategory, string productName,
+                       string productColor, double productPrice, int productStock, 
+                       string productImages, string productSize, string productDescription,
                        string productStatus, string productAddedAt)
         {
             Product_ID = productID;
