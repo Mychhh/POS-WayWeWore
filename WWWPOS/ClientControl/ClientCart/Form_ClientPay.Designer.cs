01a4dc95d19e71c116f4ac3f73ed29f411694d58
Michael <michaeldrpabaya@gmail.com>
2022-12-29 11:40:25 +0800

    payments working

--- WWWPOS/ClientControl/ClientCart/Form_ClientPay.Designer.cs
+++ WWWPOS/ClientControl/ClientCart/Form_ClientPay.Designer.cs
@@ -36,11 +36,12 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.lbl_TotalPrice = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btn_BrowseProduct = new System.Windows.Forms.Button();
+            this.btn_GoToCart = new System.Windows.Forms.Button();
             this.panel_Menu = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btn_Menu = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_GoToProducts = new System.Windows.Forms.Button();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -127,21 +128,21 @@
             this.label1.Text = "Total : ";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // btn_BrowseProduct
+            // btn_GoToCart
             // 
-            this.btn_BrowseProduct.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(198)))), ((int)(((byte)(116)))));
-            this.btn_BrowseProduct.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_BrowseProduct.FlatAppearance.BorderSize = 0;
-            this.btn_BrowseProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_BrowseProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_BrowseProduct.Location = new System.Drawing.Point(1191, 164);
-            this.btn_BrowseProduct.Margin = new System.Windows.Forms.Padding(4);
-            this.btn_BrowseProduct.Name = "btn_BrowseProduct";
-            this.btn_BrowseProduct.Size = new System.Drawing.Size(175, 49);
-            this.btn_BrowseProduct.TabIndex = 79;
-            this.btn_BrowseProduct.Text = "Cart";
-            this.btn_BrowseProduct.UseVisualStyleBackColor = false;
-            this.btn_BrowseProduct.Click += new System.EventHandler(this.btn_BrowseProduct_Click);
+            this.btn_GoToCart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(198)))), ((int)(((byte)(116)))));
+            this.btn_GoToCart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_GoToCart.FlatAppearance.BorderSize = 0;
+            this.btn_GoToCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_GoToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_GoToCart.Location = new System.Drawing.Point(1202, 173);
+            this.btn_GoToCart.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_GoToCart.Name = "btn_GoToCart";
+            this.btn_GoToCart.Size = new System.Drawing.Size(164, 32);
+            this.btn_GoToCart.TabIndex = 79;
+            this.btn_GoToCart.Text = "View cart";
+            this.btn_GoToCart.UseVisualStyleBackColor = false;
+            this.btn_GoToCart.Click += new System.EventHandler(this.btn_BrowseProduct_Click);
             // 
             // panel_Menu
             // 
@@ -191,17 +192,34 @@
             this.pictureBox1.TabIndex = 75;
             this.pictureBox1.TabStop = false;
             // 
+            // btn_GoToProducts
+            // 
+            this.btn_GoToProducts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(198)))), ((int)(((byte)(116)))));
+            this.btn_GoToProducts.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_GoToProducts.FlatAppearance.BorderSize = 0;
+            this.btn_GoToProducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_GoToProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_GoToProducts.Location = new System.Drawing.Point(1021, 173);
+            this.btn_GoToProducts.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_GoToProducts.Name = "btn_GoToProducts";
+            this.btn_GoToProducts.Size = new System.Drawing.Size(164, 32);
+            this.btn_GoToProducts.TabIndex = 83;
+            this.btn_GoToProducts.Text = "Browse product";
+            this.btn_GoToProducts.UseVisualStyleBackColor = false;
+            this.btn_GoToProducts.Click += new System.EventHandler(this.btn_GoToProducts_Click);
+            // 
             // Form_ClientPay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(19)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(1424, 788);
+            this.Controls.Add(this.btn_GoToProducts);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label16);
             this.Controls.Add(this.flPanel_ProductTotal);
             this.Controls.Add(this.panel4);
-            this.Controls.Add(this.btn_BrowseProduct);
+            this.Controls.Add(this.btn_GoToCart);
             this.Controls.Add(this.panel_Menu);
             this.Controls.Add(this.btn_Menu);
             this.Controls.Add(this.panel1);
@@ -225,12 +243,13 @@
         public System.Windows.Forms.FlowLayoutPanel flPanel_ProductTotal;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Button btn_BrowseProduct;
+        private System.Windows.Forms.Button btn_GoToCart;
         private System.Windows.Forms.Panel panel_Menu;
         private System.Windows.Forms.Button btn_Menu;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.Label lbl_TotalPrice;
+        private System.Windows.Forms.Button btn_GoToProducts;
     }
 }
